4ada96bde19b19a9abe0fa5be8f4059772e2b428
yoshimoto1146-debug <yoshimoto1146@gmail.com>
2026-01-26 20:16:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    // 提供されたAPIキーをアプリ全体で利用可能にする
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || 'AIzaSyCc6rx2TIZqp-ZFO4us0I3KBCqSpUPkPLg'),
   },
   server: {
     host: true
